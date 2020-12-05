9edec8e8f32ba061600fb602081fbe129deb883f
Arc <arcvaartmans@gmail.com>
2020-12-05 12:00:18 +0100

    javascript code toegevoegd

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-var ducky = document.getElementById("ducky");
-var scoreText = document.getElementById("score");
+var ducky = document.getElementById('ducky');
+var scoreText = document.getElementById('score');
+var background = document.getElementById('background');
 var score = 0;
+var missCounter = 0;
+var direction = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+
+
 
 function hit(){
 	score++;
-	console.log(score);
+	console.log("you have " + score + " score");
+}
+
+function miss(){
+	missCounter++;
+	if(score > 0){
+		score--;
+	}
+	else{
+		alert("game over");
+	}
+	console.log("you have " + score + " score");
+	console.log("you have " + missCounter + " misses");
 }
